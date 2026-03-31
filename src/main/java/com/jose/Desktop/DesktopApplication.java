c4a2b173377b2c310f3c59d22eee413ea7b699a2
JSkahl <josekahl333@gmail.com>
2026-03-31 14:21:31 -0300

    batman

---
+++ src/main/java/com/jose/Desktop/DesktopApplication.java
@@ -0,0 +1,13 @@
+package com.jose.Desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/Desktop/DesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.Desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
