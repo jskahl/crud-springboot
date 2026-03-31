2e990b916f2b8c894be73d971f856a9ff1a5ddf1
JSkahl <josekahl333@gmail.com>
2026-03-31 15:49:46 -0300

    feat(repository): create item repository

---
+++ src/main/java/com/jose/Desktop/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.jose.Desktop.repository;
+
+import com.jose.Desktop.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item> {
+
+}
