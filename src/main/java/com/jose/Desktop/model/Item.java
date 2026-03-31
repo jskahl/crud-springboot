a806c44287a8522f206687fe58a0b0a13d655911
JSkahl <josekahl333@gmail.com>
2026-03-31 14:22:03 -0300

    feat(model): create item model

---
+++ src/main/java/com/jose/Desktop/model/Item.java
@@ -0,0 +1,59 @@
+package com.jose.Desktop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String dateCreation;
+    private Integer itemType;
+
+    public Item() {
+    }
+
+    public Item(Long id, String name, String dateCreation, Integer itemType) {
+        this.id = id;
+        this.name = name;
+        this.dateCreation = dateCreation;
+        this.itemType = itemType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateCreation() {
+        return dateCreation;
+    }
+
+    public Integer getItemType() {
+        return itemType;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateCreation(String dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public void setItemType(Integer itemType) {
+        this.itemType = itemType;
+    }
+
+}
